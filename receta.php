cf9e153a1d1f246727c9e1fd4c3abf621ccd238b
JoseAraque99 <jaraque074@gmail.com>
2021-03-08 19:17:32 -0500

    modificación a al archivo receta.php

--- receta.php
+++ receta.php
@@ -2,9 +2,54 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Recetas</title>
   </head>
-  <body>
-    <h1>Receta</h1>
+  <body background="imagenes/fondo2.jpeg" width="100%">
+    <?php require_once "header.php"; ?>
+    <main>
+      <br>
+      <section>
+        <ol>
+          <li>Usa frutas cuyos sabores se diferencien fácilmente unos de otros.
+            Es decir, si usas nectarinas y duraznos vas a tener sabores similares,
+            igual que si usas mandarinas y naranjas. ¿Lo ideal? Te doy un ejemplo:
+            fresas, mandarinas, peras y nectarinas.Otro ejemplo: sandía, kiwi y nectarinas.
+            Puedes ir probando qué te gusta más, y claro, usando lo que tengas en la nevera.</li>
+          <li>Lo segundo a tomar en cuenta es si vas a usar algún líquido como aderezo,
+              que bien podría ser limón, jugo de naranjas o miel. En realidad casi cualquier jugo.
+              Hay quienes le ponen yogurt, lo cual puede ser una buena idea.</li>
+          <li>Luego está el azúcar. Muchas recetas de ensalada de frutas incluyen
+                azúcar en sus ingredientes. A mí me parece excesivo ¿sabes la cantidad de
+                fructosa que tienen ese montón de frutas? Utilízala sólo si de verdad es necesario,
+                y preferiblemente, sustituye por miel de abejas, pero mejor deja que sea todo
+                lo más natural posible, digo yo.</li>
+          <li>Lo normal es picar las frutas en cubos de alrededor de 1 cm de tamaño,
+              y siempre retirar las semillas en el caso de sandías, papayas, melones, etc.</li>
+          <li>Por supuesto, puedes ponerte creativo y agregarle un toque de licor a tu preparación,
+            aunque entonces estarías cerca de hacer una sangría.</li>
+        </ol>
+      </section>
+      <section>
+        <h3>Ingredientes</h3>
+        <ul>
+          <li>1 manzana</li>
+          <li>1/4 de fresas</li>
+          <li>100 gramos de piña</li>
+          <li>10 moras</li>
+          <li>1 nectarina</li>
+          <li>Leche condensada</li>
+        </ul>
+      </section>
+      <section>
+        <h3>Elaboración paso a paso</h3>
+        <ol>
+          <li>Cortar todas las frutas en cubos de no más de 1 cm de diámetro, en el caso
+          de las fresas y moras en ruedas.</li>
+          <li>Aplicar leche condensada al gusto.</li>
+        </ol>
+      </section>
+      <br>
+    </main>
+    <?php require_once "footer.php" ?>
   </body>
 </html>
